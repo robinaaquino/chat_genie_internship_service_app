bab1e50cbf96db9f627ab96a07c0dbbe7c75a1ae
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-04-05 16:10:19 +0800

    fix: test2

--- src/backend.ts
+++ src/backend.ts
@@ -3,10 +3,12 @@ import { Cloudinary } from "@cloudinary/url-gen";
 
 const httpLink = new HttpLink({
   uri: process.env.VUE_APP_URI,
-  credentials: "include",
   headers: {
-    "Content-Type": "application/graphql",
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": "true",
   },
+  fetch,
 });
 
 export const apolloClient = new ApolloClient({
